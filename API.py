f084076ef34ab7b3e1be53e0a6dd837a0e4ac4a3
Shaan Chauhan <shaanbroncos@gmail.com>
2023-03-24 13:15:36 -0600

    Requesting Spotify Artist
    Need new bearer token to run

--- API.py
+++ API.py
@@ -1,10 +1,17 @@
+#Using Postman Web Api to create code snipet requests for Spotify API
+#https://web.postman.co/workspace/My-Workspace~fb306756-5e0a-4fa0-8c53-6c32df7a4f16/request/26533462-50871f04-4144-48d4-ad70-f3017af37e4a?ctx=code
+
+
+import os
 import requests
 
 url = "https://api.spotify.com/v1/search?q=Travis%20Scott&type=artist"
 
+
+
 payload={}
 headers = {
-  'Authorization': 'Bearer BQB2AYxIN7-whoLRNb9p2gABPwgMiCwGn5NoHHV8S8ArdUSEAlSII7G50TX5Zkx9A13kM0T60pcTYluWurDb2O9BSGHtMiJ0U38T0uEAS6mN7-61IykGNx_FwVc-e2Os3LlqSVmqsfWN3RySjWZNvJyXWgvdm6P2ZugPA3qcJ1v1kBF2TUya0LjXGzRnvcWU5wjP'
+  'Authorization': 'BQCUruHZ66m7HpjkCn-nTteqgDPoAmI2fUPAJaGHW0KdCfGx3qbSi5RxQgvEJBwSjf9gqB2WA2rN1V0XRZzflBa-dHwYqT5Rjoi0WDMF03PlN4SxiYotT9FPtE7jP_qkcSTEEcfBZzJAA2AitHYN1wxeph7Kr1O6ocd3iYUR9z0ukrdQma5LUurXZLDbsNy5PgqU'
 }
 
 response = requests.request("GET", url, headers=headers, data=payload)
